732715d8059af3109886f4099e5c8fb0b0568c70
andrews <andrews@wdhouse.com.br>
2021-01-20 23:42:17 -0300

    Painel admin with login

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 var conn = require('./../inc/db')
 var reservations = require('./../inc/reservations')
-var express = require('express');
+var contacts = require('./../inc/contacts')
+var express = require('express')
 var menus = require('./../inc/menus')
-var router = express.Router();
+var router = express.Router()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -16,7 +17,24 @@ router.post('/', function(req, res, next) {
 });
 
 router.get('/contacts', function(req, res, next) {
-	res.render('contact', { title: 'Contato - Restaurante Saboroso!', background: 'images/img_bg_3.jpg', h1: 'Entre em contato' })
+	contacts.render(req, res)
+});
+
+router.post('/contacts', function(req, res, next) {
+	if (!req.body.name){
+		contacts.render(req, res, 'Digite o nome')
+	} else if(!req.body.email){
+		contacts.render(req, res, 'Digite o email')
+	}else if(!req.body.message){
+		contacts.render(req, res, 'Digite a mensagem')
+	} else{
+		contacts.save(req.body).then(results =>{
+			req.body = {}
+			contacts.render(req, res, null, 'Contato enviado com sucesso')
+		}).catch(err =>{
+			contacts.render(req, res, err.message)
+		})
+	}
 });
 
 router.get('/menus', function(req, res, next) {
